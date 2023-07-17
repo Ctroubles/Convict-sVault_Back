5f027c3b6e7affe0fc9a1df79db229d99df98e46
troubles <cesar.troubles@gmail.com>
2023-07-17 12:25:22 -0500

    test depl 22

--- src/db.js
+++ src/db.js
@@ -12,7 +12,7 @@ main().catch(err => console.log(err));
 
 async function main() {
     mongoose.set('strictQuery', true);
-    await mongoose.connect(`mongodb+srv://labodegadelreo122:6VXwlnue09MPHlOU@cluster0.193s2bq.mongodb.net/LaBodegaDelReo_DB?retryWrites=true&w=majority`);
+    await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/${DB_NAME}?retryWrites=true&w=majority`);
     console.log("connected to db")
   }
   
