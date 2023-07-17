796114eec8be11679879f9a3af70646b97c7d3ac
troubles <cesar.troubles@gmail.com>
2023-07-17 12:31:38 -0500

    test depl 24

--- src/db.js
+++ src/db.js
@@ -1,14 +1,18 @@
-require('dotenv').config();
-const mongoose= require("mongoose");
+// require('dotenv').config();
+// const mongoose= require("mongoose");
 
-const {
-  DB_NAME, DB_PASSWORD,
-} = process.env;
+// const {
+//     DB_USER,
+//     DB_PASSWORD,
+//     DB_CLUSTER,
+//     DB_NAME
+//   } = process.env;
 
-main().catch(err => console.log(err));
+// main().catch(err => console.log(err));
 
-
-async function main() {
-  mongoose.set('strictQuery', true);
-  await mongoose.connect(`mongodb+srv://tex_user:${DB_PASSWORD}@cluster0aws.reehusu.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,{ useNewUrlParser: true });
-}
+// async function main() {
+//     mongoose.set('strictQuery', true);
+//     await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/${DB_NAME}?retryWrites=true&w=majority`);
+//     console.log("connected to db")
+// }
+  
