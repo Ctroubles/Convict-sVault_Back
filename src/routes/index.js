bc926dae1abee2d49e05d42077e1343045524041
troubles <cesar.troubles@gmail.com>
2023-07-17 12:49:38 -0500

    final deploy

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,19 @@
 const { Router } = require('express');
-// const productRoutes= require("./productRoutes")
-// const uploadProducts= require("../controllers/CloudinaryControl")
-// const userRoutes= require("./userRoutes")
-// const cartRoutesc = require('./cartHandlerRoutes');
-// const pagosRoutes= require("./pagosRoutes");
-// const transactionRoutes = require('./transactionRoutes');
+const productRoutes= require("./productRoutes")
+const uploadProducts= require("../controllers/CloudinaryControl")
+const userRoutes= require("./userRoutes")
+const cartRoutesc = require('./cartHandlerRoutes');
+const pagosRoutes= require("./pagosRoutes");
+const transactionRoutes = require('./transactionRoutes');
 
 const router = Router();
 
-// router.use("/products", productRoutes)
-// router.use("/upload", uploadProducts)
-// router.use("/users", userRoutes)
-// router.use("/cart", cartRoutesc)
-// router.use("/pagos", pagosRoutes)
-// router.use("/transactions", transactionRoutes)
+router.use("/products", productRoutes)
+router.use("/upload", uploadProducts)
+router.use("/users", userRoutes)
+router.use("/cart", cartRoutesc)
+router.use("/pagos", pagosRoutes)
+router.use("/transactions", transactionRoutes)
 
 
 module.exports = router;
