752ff02a6a1d071e67fa00f9913ad5fe09d20d75
kan4crooo <kevinacr17@gmail.com>
2023-10-03 20:36:01 -0500

    epayco en la cuenta de cata v1.3

--- src/Epeyco/crearSession.js
+++ src/Epeyco/crearSession.js
@@ -11,7 +11,7 @@ async function makePaymentWithToken(token, datos) {
         currency:datos.currency || 'cop',
         amount:datos.amount || '10000',
         country:datos.country || 'CO',
-        test:false,
+        test:datos.test || false,
         ip:datos.ip || '186.97.212.162'
       };
   
