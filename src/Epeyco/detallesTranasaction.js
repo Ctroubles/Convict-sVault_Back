22066534c480075778244923e5ae7d7da06eeebe
kan4crooo <kevinacr17@gmail.com>
2023-10-31 03:01:12 -0500

    detallesTransaction para la reduccion del stock prueba #7 ya funciona pero esta hardcodeado el referencePayco

--- src/Epeyco/detallesTranasaction.js
+++ src/Epeyco/detallesTranasaction.js
@@ -5,7 +5,7 @@ async function transactionDetail(token) {
     const url_apify = 'https://apify.epayco.co'; // Verifica el URL correcto
     const data = JSON.stringify({
       "filter": {
-        "referencePayco": 172885281
+        "referencePayco": 172885365
       }
     });
 
