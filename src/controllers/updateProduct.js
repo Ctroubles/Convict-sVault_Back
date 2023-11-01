918034fa3e96d9fbc11fac182f09e6db7dd9949d
kan4crooo <kevinacr17@gmail.com>
2023-11-01 18:51:40 -0500

    cambios en updateProduct.js

--- src/controllers/updateProduct.js
+++ src/controllers/updateProduct.js
@@ -3,6 +3,9 @@ const Products = require("../models/products_models");
 exports.updateProducts = async (id, data) => {
   const product = await Products.findOne({ _id: id });
   if (!product) throw `No product found with that ${id}`;
+
+  console.log('Datos antes de la actualización:', product);
+
   product.name = data.name;
   product.price = data.price;
   product.image = data.image;
@@ -10,5 +13,8 @@ exports.updateProducts = async (id, data) => {
   product.category = data.category;
   product.stock = data.stock;
   product.isActive = data.isActive;
+
+  console.log('Datos después de la actualización:', product);
+
   return await product.save();
 };
