a5d6ae061e3096d59b4fe74f95c7842ab71ed4ef
kan4crooo <kevinacr17@gmail.com>
2023-10-31 19:14:47 -0500

    se esta actualizando el stock 2 veces asi que comente detallesTransaction.js

--- src/Epeyco/detallesTranasaction.js
+++ src/Epeyco/detallesTranasaction.js
@@ -1,31 +1,31 @@
-const axios = require('axios');
+// const axios = require('axios');
 
-async function transactionDetail(token) {
-  try {
-    const url_apify = 'https://apify.epayco.co'; // Verifica el URL correcto
-    const data = JSON.stringify({
-      "filter": {
-        "referencePayco": 172885365
-      }
-    });
+// async function transactionDetail(token) {
+//   try {
+//     const url_apify = 'https://apify.epayco.co'; // Verifica el URL correcto
+//     const data = JSON.stringify({
+//       "filter": {
+//         "referencePayco": 172885365
+//       }
+//     });
 
-    const config = {
-      method: 'get',
-      url: `${url_apify}/transaction/detail`,
-      headers: { 
-        'Authorization': `Bearer ${token}`,
-        'Content-Type': 'application/json'
-      },
-      data: data
-    };
+//     const config = {
+//       method: 'get',
+//       url: `${url_apify}/transaction/detail`,
+//       headers: { 
+//         'Authorization': `Bearer ${token}`,
+//         'Content-Type': 'application/json'
+//       },
+//       data: data
+//     };
 
-    const response = await axios(config); // Utiliza async/await para obtener la respuesta
-    console.log(JSON.stringify(response.data));
-    return response.data; // Devuelve la respuesta
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-}
+//     const response = await axios(config); // Utiliza async/await para obtener la respuesta
+//     console.log(JSON.stringify(response.data));
+//     return response.data; // Devuelve la respuesta
+//   } catch (error) {
+//     console.log(error);
+//     throw error;
+//   }
+// }
 
-module.exports = transactionDetail;
+// module.exports = transactionDetail;
--- src/routes/index.js
+++ src/routes/index.js
@@ -34,14 +34,11 @@ router.use("/transactions", transactionRoutes)
 
 router.post('/confirmation-epayco', (req, res) => {
   try {
-    // Obtén el valor de x_ref_payco de la URL de confirmación
     const x_ref_payco = req.query.x_ref_payco;
     console.log(x_ref_payco)
     
-    // Asegúrate de que los datos se almacenan en un objeto como en el ejemplo anterior
     const datosEpayco = {
       x_ref_payco: x_ref_payco,
-      // Otras propiedades si es necesario
     };
 
     // Envía x_ref_payco en la respuesta JSON
